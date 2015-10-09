141bc11a73a1d6505f13b8ce94a23adfd55fe09f
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2015-10-09 13:51:20 +0200

    Form fields for the 2 first pages

---
+++ forms/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class ContactForm1(forms.Form):
+	possible_types = ['Testing','Development','Production']
+    type = ChoiceField(choices=possible_types, label="Project Type")
+
+class ContactForm2(forms.Form):
+    start_date = DateField(label="Project Start Date")
+    end_date = DateField(label="Project End Date")
+    ongoing_projects = CharField(max_length=1000, label="On going related projects")
+    past_projects = CharField(max_length=1000, label="Past related projects")
+	possible_editing_styles = ['Plain text','latex']
+    editing_styles = ChoiceField(choices=possible_editing_styles, label="Choose your editing style")
