977425e158f7de07568f7a2f1c52ea87266d9a15
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2015-10-12 11:28:40 +0200

    urls.py fixed, new package for formular.views

--- formular/urls.py
+++ formular/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns
 
-from formular.forms import ProposalForm1, ProposalForm2
-from formular.views import ProposalWizard
 
-urlpatterns = patterns('',
-    (r'^$', ProposalWizard.as_view([ProposalForm1, ProposalForm2])),
-)
+from formular.views import ProposalWizard
+from formular.forms import ProposalForm1, ProposalForm2
+
+urlpatterns = [
+    url(r'^$', ProposalWizard.as_view([ProposalForm1, ProposalForm2])),
+]
--- formular/views.py
+++ formular/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render_to_response
-from django.contrib.formtools.wizard.views import SessionWizardView
+from formtools.wizard.views import SessionWizardView
 
-class ContactWizard(SessionWizardView):
+class ProposalWizard(SessionWizardView):
     def done(self, form_list, **kwargs):
-        return render_to_response('wizard_form.html', {
+        return render_to_response('done.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
