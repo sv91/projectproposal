f364425ebdc6a4c144533224e691cab3c46e3a2c
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2015-10-30 15:15:05 +0100

    Remove editing_style

--- formular/models.py
+++ formular/models.py
@@ -9,8 +9,6 @@ class Proposal2(models.Model):
 	end_date = models.DateField()
 	ongoing_projects = models.CharField(max_length=1000)
 	past_projects = models.CharField(max_length=1000)
-	possible_editing_styles = [['plain','Plain text'],['latex','Latex']]
-	editing_styles = models.CharField(max_length=5,choices=possible_editing_styles)
 	
 class Proposal3(models.Model):
 	pi = models.CharField(max_length=100)
